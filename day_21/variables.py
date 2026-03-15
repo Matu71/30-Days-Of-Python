d1f40f3fa149f34ab5d80e9eaf044d647576cfb8
Matu71 <200363731+Matu71@users.noreply.github.com>
2026-03-15 11:59:47 +0200

    Update variables.py

--- day_21/variables.py
+++ day_21/variables.py
@@ -1,1 +1,13 @@
 #Day 2: 30 Days of python programming
+first_name= 'Mattias'
+last_name = 'Rannik'
+Full_name = 'Mattias Rannik'
+country = 'Eesti'
+Linn = 'Tallinn'
+Age = 20
+Year = 2026
+is_married = False
+is_true = 'no'
+is_light_on = 'yes'
+height , weight = 180 , 80
+
